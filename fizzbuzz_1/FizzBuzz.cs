e422bf6aaabc19a344c266ad108dc5004501d6e7
Pawel Zimolag <pawel.zimolag02@gmail.com>
2024-02-28 19:34:51 +0100

    Dodaj pliki projektów.

---
+++ fizzbuzz_1/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz_1
+{
+    internal class FizzBuzz
+    {
+        private int max_nr;
+        public FizzBuzz(int number) {
+            max_nr = number;
+        }
+        public void display() {
+            for (int i = 1; i <= max_nr; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 3 != 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ fizzbuzz_1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace fizzbuzz_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizz = new FizzBuzz(20);
+            fizz.display();
+        }
+    }
+}
